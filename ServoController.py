447c771183b5bad4eac51f132d098bbd83160e08
Jonas Wedin <jonas@lechuck.se>
2015-04-19 22:56:55 +0200

    Added tests

--- ServoController.py
+++ ServoController.py
@@ -51,7 +51,7 @@ class ServoController(GpioController.GpioController):
         self._currentDirection = ANGLE_180
         self._currentDirectionDegrees = DEGREE_180
         self._updateAngle()
-    
+
     def rotateTo(degrees):
         if(int(degrees) < 0 or int(degrees) > 180):
             raise ValueError ("input should be between 0 and 180 degrees (int), got: %s" % degrees)
@@ -75,9 +75,12 @@ class ServoController(GpioController.GpioController):
             self._currentDirection = convDeg
             self._currentDirectionDegrees = newDeg
             self._updateAngle()
-    
+
     def getDegrees():
         return self._currentDirectionDegrees
+
+    def isRunning():
+        return self._running
 
     def _updateAngle():
         self._pwm.ChangeDutyCycle(self._currentDirection)
@@ -87,4 +90,111 @@ class ServoController(GpioController.GpioController):
         #y = kx + m
         returnValue = ( k * degress ) + self.ANGLE_ZERO
         return returnValue
+
+# Tests
+if __name__ == '__main__':
+    print("======= ServoController ========")
+    print("Please ensure that you have nothing connected on pin 21 (BCM)  of your RasPi")
+    print("as this script will use that pin for tests!")
+    sure == raw_input("Are you sure nothing is connected to pin 21? Y/n :")
+    if sure == "Y":
+        print("")
+    else:
+        print("Aborting tests.")
+        exit(0)
+    print("Starting tests:")
+    print("Creating servo")
+    # Creating a servo that does not start when creating it
+    servo = ServoController(21, False)
+    print("Ensure that it isn't started since we passed directStart = False")
+    if servo.isRunning() == True:
+        print("isRunning failed")
+        exit(1)
+    print("OK")
+    print("Check that degrees = 90") 
+    if servo.getDegrees() != 90:
+        print("Was not in neutral angle from beginning")
+        exit(1)
+    print("OK")
+    print("Start it and check that its running")
+    servo.start()
+    if servo.isRunning() == False:
+        print("Started but isRunning was false.")
+        exit(1)
+    print("OK")
+    print("Rotate it to 0 and check the getDegrees")
+    servo.rotateToZero()
+    if servo.getDegrees() != 0:
+        print("It was not 0 degrees")
+        exit(1)
+    print("OK")
+    print("Rotate it to 180 degrees and check")
+    servo.rotateTo180()
+    if servo.getDegrees() != 180:
+        print("Could not rotate to 180 degrees")
+        exit(1)
+    print("OK")
+    print("Rotate it back to neutral")
+    servo.rotateToNeutral()
+    if servo.getDegrees() != 90:
+        print("Could not rotate back to neutral")
+        exit(1)
+    print("OK")
+    print("Use rotateBy to rotate another 10 degrees to 100")
+    servo.rotateBy(10)
+    if servo.getDegrees() != 100:
+        print("Could not rotate to 100")
+        exit(1)
+    print("OK")
+    print("Use rotateBy to rotate back 20 degrees to 80")
+    servo.rotateBy(-20)
+    if servo.getDegrees() != 80:
+        print("Could not rotateback to 80")
+        exit(1)
+    print("OK")
+    print("Use rotateBy to go under 0 , should stay at 0")
+    servo.rotateBy(-120)
+    if servo.getDegrees() != 0:
+        print("rotateBy(-120) did not give 0 degrees")
+        exit(1)
+    print("OK")
+    print("Use rotateBy to go over 180 , should stay at 180")
+    servo.rotateTo(300)
+    if servo.getDegrees() != 180:
+        print("RotateBy(300) did not give 180")
+        exit(1)
+    print("Use rotateTo to 45 degrees")
+    servo.rotateTo(45)
+    if servo.getDegrees() != 45:
+        print("Could not rotateTo 45 degrees")
+        exit(1)
+    print("OK")
+    print("Test stopping the servo")
+    servo.stop()
+    if servo.isRunning() != False:
+        print("Stop did not cause is running to be false")
+        exit(1)
+    print("testing Rotate to with bad values, should produce ValueError")
+    print("testing with negative value")
+    passed = False
+    try:
+        servo.rotateTo(-20)
+    except ValueError, e:
+        print("OK")
+        passed = True
+
+    if passed != False:
+       print("Could not set negative value")
+       exit(1)
+    passed = False
+    try:
+        servo.rotateTo(300)
+    except ValueError, e:
+        print("OK")
+        passed = True
+    if passed is False:
+        print("Could not set to 300")
+        exit(1)
+    print("All tests passed")
+    print("===============")
 
