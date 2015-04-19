b48f7e7b78b6ab4081d1460527ad8f2a7b203ca1
Jonas Wedin <jonas@lechuck.se>
2015-04-19 20:15:38 +0200

    Added stub of functions

--- ServoController.py
+++ ServoController.py
@@ -3,11 +3,70 @@ import GpioController
 from sys import exit
 
 class ServoController(GpioController.GpioController):
-   """
+    """
    Handles general servos (not continuos ones)
    """
 
    #constants
+    DEFAULT_FREQ = 50
+    ANGLE_NEUTRAL=7.5
+    DEGREE_NEUTRAL=90
+    ANGLE_ZERO=2.5
+    DEGREE_ZERO = 0
+    ANGLE_180=12.5
+    DEGREE_180 = 180
 
-   def __init__(self,controllPin,):
-       
+    def __init__(self,controllPin,directStart=True,boardmode="BCM"):
+        super(ServoController, self).__init__([controllPin], boardmode)
+        # Setup
+        io.setup(controllPin, io.OUT)
+        self._pwm = io.PWM(controllPin, DEFAULT_FREQ)
+        self._currentDirection = ANGLE_NEUTRAL
+        self._currentDirectionDegrees = DEGREE_NEUTRAL 
+        self._running = False
+        if directStart is True:
+            self.start()
+
+    def start():
+        if self._running is False:
+            self._pwm.start(self._currentDirection)
+            self._running = True
+
+    def stop():
+        if self._running is True:
+            self._pwm.stop()
+            self._running = False
+
+    def rotateToNeutral():
+        self._currentDirection = ANGLE_NEUTRAL
+        self._currentDirectionDegrees = DEGREE_NEUTRAL
+        self._updateAngle()
+
+    def rotateToZero():
+        self._currentDirection = ANGLE_ZERO
+        self._currentDirectionDegrees = DEGREE_ZERO
+        self._updateAngle()
+
+    def rotateTo180():
+        self._currentDirection = ANGLE_180
+        self._currentDirectionDegrees = DEGREE_180
+        self._updateAngle()
+    
+    def rotateTo(degrees):
+        if(int(degrees) < 0 or int(degrees) > 180):
+            raise ValueError ("input should be between 0 and 180 degrees (int), got: %s" % degrees)
+        convDegrees = self._convertDegrees(degrees)
+        self._currentDirection = convDegrees
+        self._currentDirectionDegrees = degrees
+        self._updateAngle()
+
+    def rotateBy(degrees):
+        if degrees != 0:
+            absDegrees = abs(degrees)
+            if degrees < 0:
+                newDeg = self._currentDirectionDegrees - absDegrees
+                
+
+
+    def _updateAngle():
+        self._pwm.ChangeDutyCycle(self._currentDirection)
