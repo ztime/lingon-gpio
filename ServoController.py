0cfa7e5e7e037a7b82e6448bb6d86b8bd65443a6
Jonas Wedin <jonas@lechuck.se>
2015-04-19 21:49:42 +0000

    fixed bugs, all tests passing

--- ServoController.py
+++ ServoController.py
@@ -1,5 +1,6 @@
 import RPi.GPIO as io
 import GpioController
+import time
 from sys import exit
 
 class ServoController(GpioController.GpioController):
@@ -20,39 +21,39 @@ class ServoController(GpioController.GpioController):
         super(ServoController, self).__init__([controllPin], boardmode)
         # Setup
         io.setup(controllPin, io.OUT)
-        self._pwm = io.PWM(controllPin, DEFAULT_FREQ)
-        self._currentDirection = ANGLE_NEUTRAL
-        self._currentDirectionDegrees = DEGREE_NEUTRAL 
+        self._pwm = io.PWM(controllPin, self.DEFAULT_FREQ)
+        self._currentDirection = self.ANGLE_NEUTRAL
+        self._currentDirectionDegrees = self.DEGREE_NEUTRAL 
         self._running = False
         if directStart is True:
             self.start()
 
-    def start():
+    def start(self):
         if self._running is False:
             self._pwm.start(self._currentDirection)
             self._running = True
 
-    def stop():
+    def stop(self):
         if self._running is True:
             self._pwm.stop()
             self._running = False
 
-    def rotateToNeutral():
-        self._currentDirection = ANGLE_NEUTRAL
-        self._currentDirectionDegrees = DEGREE_NEUTRAL
+    def rotateToNeutral(self):
+        self._currentDirection = self.ANGLE_NEUTRAL
+        self._currentDirectionDegrees = self.DEGREE_NEUTRAL
         self._updateAngle()
 
-    def rotateToZero():
-        self._currentDirection = ANGLE_ZERO
-        self._currentDirectionDegrees = DEGREE_ZERO
+    def rotateToZero(self):
+        self._currentDirection = self.ANGLE_ZERO
+        self._currentDirectionDegrees = self.DEGREE_ZERO
         self._updateAngle()
 
-    def rotateTo180():
-        self._currentDirection = ANGLE_180
-        self._currentDirectionDegrees = DEGREE_180
+    def rotateTo180(self):
+        self._currentDirection = self.ANGLE_180
+        self._currentDirectionDegrees = self.DEGREE_180
         self._updateAngle()
 
-    def rotateTo(degrees):
+    def rotateTo(self,degrees):
         if(int(degrees) < 0 or int(degrees) > 180):
             raise ValueError ("input should be between 0 and 180 degrees (int), got: %s" % degrees)
         convDegrees = self._convertDegrees(degrees)
@@ -60,7 +61,7 @@ class ServoController(GpioController.GpioController):
         self._currentDirectionDegrees = degrees
         self._updateAngle()
 
-    def rotateBy(degrees):
+    def rotateBy(self,degrees):
         if degrees != 0:
             absDegrees = abs(degrees)
             if degrees < 0:
@@ -76,27 +77,28 @@ class ServoController(GpioController.GpioController):
             self._currentDirectionDegrees = newDeg
             self._updateAngle()
 
-    def getDegrees():
+    def getDegrees(self):
         return self._currentDirectionDegrees
 
-    def isRunning():
+    def isRunning(self):
         return self._running
 
-    def _updateAngle():
+    def _updateAngle(self):
         self._pwm.ChangeDutyCycle(self._currentDirection)
 
-    def _convertDegrees(degrees):
+    def _convertDegrees(self,degrees):
         k = (self.ANGLE_180 - self.ANGLE_ZERO) / (self.DEGREE_180 - self.DEGREE_ZERO)
         #y = kx + m
-        returnValue = ( k * degress ) + self.ANGLE_ZERO
+        returnValue = ( k * int(degrees) ) + self.ANGLE_ZERO
         return returnValue
 
 # Tests
 if __name__ == '__main__':
+    sleepTime = 2
     print("======= ServoController ========")
     print("Please ensure that you have nothing connected on pin 21 (BCM)  of your RasPi")
     print("as this script will use that pin for tests!")
-    sure == raw_input("Are you sure nothing is connected to pin 21? Y/n :")
+    sure = raw_input("Are you sure nothing is connected to pin 21? Y/n :")
     if sure == "Y":
         print("")
     else:
@@ -118,62 +120,74 @@ if __name__ == '__main__':
     print("OK")
     print("Start it and check that its running")
     servo.start()
+    time.sleep(sleepTime)
     if servo.isRunning() == False:
         print("Started but isRunning was false.")
         exit(1)
     print("OK")
     print("Rotate it to 0 and check the getDegrees")
     servo.rotateToZero()
+    time.sleep(sleepTime)
     if servo.getDegrees() != 0:
         print("It was not 0 degrees")
         exit(1)
     print("OK")
     print("Rotate it to 180 degrees and check")
     servo.rotateTo180()
+    time.sleep(sleepTime)
     if servo.getDegrees() != 180:
         print("Could not rotate to 180 degrees")
         exit(1)
     print("OK")
     print("Rotate it back to neutral")
     servo.rotateToNeutral()
+    time.sleep(sleepTime)
     if servo.getDegrees() != 90:
         print("Could not rotate back to neutral")
         exit(1)
     print("OK")
     print("Use rotateBy to rotate another 10 degrees to 100")
     servo.rotateBy(10)
+    time.sleep(sleepTime)
     if servo.getDegrees() != 100:
         print("Could not rotate to 100")
         exit(1)
     print("OK")
     print("Use rotateBy to rotate back 20 degrees to 80")
     servo.rotateBy(-20)
+    time.sleep(sleepTime)
     if servo.getDegrees() != 80:
         print("Could not rotateback to 80")
         exit(1)
     print("OK")
     print("Use rotateBy to go under 0 , should stay at 0")
     servo.rotateBy(-120)
+    time.sleep(sleepTime)
     if servo.getDegrees() != 0:
         print("rotateBy(-120) did not give 0 degrees")
         exit(1)
     print("OK")
     print("Use rotateBy to go over 180 , should stay at 180")
-    servo.rotateTo(300)
+    servo.rotateBy(300)
+    time.sleep(sleepTime)
     if servo.getDegrees() != 180:
         print("RotateBy(300) did not give 180")
         exit(1)
+    print("OK")
     print("Use rotateTo to 45 degrees")
     servo.rotateTo(45)
+    time.sleep(sleepTime)
     if servo.getDegrees() != 45:
         print("Could not rotateTo 45 degrees")
         exit(1)
     print("OK")
     print("Test stopping the servo")
     servo.stop()
+    time.sleep(sleepTime)
     if servo.isRunning() != False:
         print("Stop did not cause is running to be false")
         exit(1)
+    print("OK")
     print("testing Rotate to with bad values, should produce ValueError")
     print("testing with negative value")
     passed = False
@@ -181,20 +195,16 @@ if __name__ == '__main__':
         servo.rotateTo(-20)
     except ValueError, e:
         print("OK")
-        passed = True
-
-    if passed != False:
-       print("Could not set negative value")
-       exit(1)
-    passed = False
+    servo.cleanup()
+    print("testing with a big postive value")
+    #new servo
+    servo = ServoController(21)
     try:
         servo.rotateTo(300)
     except ValueError, e:
         print("OK")
-        passed = True
-    if passed is False:
-        print("Could not set to 300")
-        exit(1)
+
     print("All tests passed")
     print("===============")
+    servo.cleanup()
 
