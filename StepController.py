c1ecbc25e032dec39702418636c8fdf6b3c45395
Jonas Wedin <jonas@lechuck.se>
2016-08-31 14:58:12 +0200

    Added test

--- StepController.py
+++ StepController.py
@@ -53,3 +53,33 @@ class StepController(GpioController.GpioController):
             setStep(1, 0, 1, 0)
             time.sleep(delay)
 
+if __name__ == '__main__':
+    print("============= Stepper motor controller ===============")
+    print("Is everything connected? Observe that trigger pin is ")
+    print("live directly at startup")
+    sure = raw_input("Do you want to continue? Y/n: ")
+    if sure == "Y":
+        print("")
+    else:
+        print("Aborting")
+        exit(0)
+
+    print("Running test:")
+    trigger = raw_input("What is the trigger pin?")
+    pink = raw_input("What is the pink pin?")
+    orange = raw_input("What is the orange pin?")
+    yellow = raw_input("What is the yellow pin?")
+    blue = raw_input("What is the blue pin?")
+
+    # create new controller
+    print("Starting up...")
+    time.sleep(1000)
+    stepper = StepController(int(trigger),int(pink),int(orange),int(yellow),int(blue))
+    #driving forward 
+    print("Going forward 100 steps, 5 mil delay")
+    stepper.forward(5, 100)
+    print("Going backwards 100 steps, 5 mil delay")
+    stepper.backward(5, 100)
+    #done
+    stepper.cleanup()
+
